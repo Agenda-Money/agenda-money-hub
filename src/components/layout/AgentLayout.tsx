31ac6d872e0f6939b8fe1e3e49c76a040f28b65e
bennydan7 <owusub243@gmail.com>
2026-01-31 10:27:36 +0000

    feat: create AgentLayout component for agent portal

---
+++ src/components/layout/AgentLayout.tsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+import { Outlet } from "react-router-dom";
+import { AgentSidebar } from "./AgentSidebar";
+import { Header } from "./Header";
+
+export default function AgentLayout() {
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+
+  return (
+    <div className="min-h-screen bg-background flex">
+      <AgentSidebar isOpen={sidebarOpen} onToggle={() => setSidebarOpen(!sidebarOpen)} />
+      
+      <div className="flex-1 flex flex-col lg:ml-0">
+        <Header onMenuClick={() => setSidebarOpen(!sidebarOpen)} />
+        
+        <main className="flex-1 p-6 overflow-y-auto">
+          <Outlet />
+        </main>
+      </div>
+    </div>
+  );
+}
